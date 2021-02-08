7a5bee33064a8fa87ce86ca7069e88eaab328e7f
TristanEvangelista <tristan.evangelista@obf.ateneo.edu>
2021-02-08 16:17:34 +0700

    added yarn stuff

---
+++ build/precache-manifest.c96bd5f68a084a5081537e693304e40c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd62fcfd1bcaa5f20543a9d5da14ecd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "899dd3b84bc8c7d5f4a0",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "efc3e85f4877f790bd14",
+    "url": "/static/js/2.3bb8dbe2.chunk.js"
+  },
+  {
+    "revision": "899dd3b84bc8c7d5f4a0",
+    "url": "/static/js/main.1dfd7202.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e5d6f01d956b94eda002ad8b3e3e31ec",
+    "url": "/static/media/profile.e5d6f01d.jpg"
+  }
+]);
